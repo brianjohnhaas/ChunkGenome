1357760bd2d11893fc85efade3b4bb5f7292a5ae
Brian Haas <brianjohnhaas@gmail.com>
2023-02-11 14:08:12 -0500

    add example data

---
+++ example/run_create_chunks.sh
@@ -0,0 +1,1 @@
+../create_chunks.py --genome_fa chr10p.10M.genome.fa --chunks chunks.tsv -o chunked --gene_annot_gtf chr10p.10M.annot.gtf
---
+++ example/run_define_chunks.sh
@@ -0,0 +1,1 @@
+../define_chunks.py --genome_fa chr10p.10M.genome.fa --max_chunk_size 2000000 --gene_annot_gtf chr10p.10M.annot.gtf --gene_spans chr10p.10M.gene_spans --N_regions chr10p.10M.N_ranges -o chunks.tsv
